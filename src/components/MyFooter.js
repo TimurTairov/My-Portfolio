27f9a3cc75fdedced857406867f66aed26fce6d9
Timur Tairov <tairovtk@gmail.com>
2023-11-05 16:08:17 +0300

    my portfolio

--- src/components/MyFooter.js
+++ src/components/MyFooter.js
@@ -8,7 +8,13 @@ const MyFooter = () => {
     <footer className="my-footer">
       <Container className="footer-container">
         <div className="container-item">
-          <a href="#"> <AiFillGithub className="icon" /> GitHub</a>
+          <a href="https://github.com/TimurTairov/my-portfolio"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <AiFillGithub className="icon" />
+            GitHub
+          </a>
         </div>
         <div className="container-item">
           <p className="container-item">© All Rights Reserved 2023™</p>
